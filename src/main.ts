86f1e525164289abe0f8a20d80f676532d553f38
Yu Nak <sunfriendo@gmail.com>
2022-04-04 15:25:09 -0500

    Media queries added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import "./scripts/lazy.js";
 let header = document.querySelector("header");
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 
+window.addEventListener("scroll", function() {
+  let windowPosition = this.window.scrollY > 0;
+  header.classList.toggle("active", this.window.scrollY > 0);
+});
+
 hamburgerMenu.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
